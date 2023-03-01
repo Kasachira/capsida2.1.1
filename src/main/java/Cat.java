dcf447a06d35da794e9303322b14a8682a0853d7
kasachita <kasachita11@gmail.com>
2023-03-01 16:08:35 +0900

    hapsida 2.1.1

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String say;
+
+    public String getMessage() {
+        return say;
+    }
+
+    public void setMessage(String message) {
+        this.say = message;
+    }
+}
